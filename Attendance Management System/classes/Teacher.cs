71b25d07c313b75d2c01a1d90c8aee0dd656c7c4
asmaa elfatayry <asmaa.elfatayry22@gmail.com>
2024-02-15 20:42:50 +0200

    build the structure of user class and try retrive data from xml file

---
+++ Attendance Management System/classes/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attendance_Management_System.classes
+{
+    public class Teacher : User
+    {
+       
+        public Teacher(string id, string firstName, string lastName, int age, string email, string password, string phone, string address)
+            : base(id, firstName, lastName, age, email, password, phone, address, "teacher") 
+        {
+          
+        }
+        public override string ToString()
+        {
+            return $"Teacher - {base.ToString()}";
+        }
+    }
+}
